b30c14a099a405780cc0b5d66a8cc13ae056faf0
gong_hero <46081369+gonghaochen@users.noreply.github.com>
2022-07-29 10:36:05 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from bokeh.plotting import figure
 
 x = [1, 2, 3, 4, 5]
@@ -12,3 +13,9 @@ p = figure(
 p.line(x, y, legend_label='Trend', line_width=2)
 
 st.bokeh_chart(p, use_container_width=True)
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
