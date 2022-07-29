0c92b3fde202385871784666b18c126f4df84462
gong_hero <46081369+gonghaochen@users.noreply.github.com>
2022-07-29 11:45:07 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -25,28 +25,28 @@ st.dataframe(df)  # Same as st.write(df)
 
 
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return mysql.connector.connect(
-         host="localhost",
-         user="root",
-         passwd="root",
-         database="enegry") #(**st.secrets["mysql"])
+# # Initialize connection.
+# # Uses st.experimental_singleton to only run once.
+# @st.experimental_singleton
+# def init_connection():
+#     return mysql.connector.connect(
+#          host="localhost",
+#          user="root",
+#          passwd="root",
+#          database="enegry") #(**st.secrets["mysql"])
 
-conn = init_connection()
+# conn = init_connection()
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# # Perform query.
+# # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+# @st.experimental_memo(ttl=600)
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
 
-rows = run_query("SELECT * from city_map;")
+# rows = run_query("SELECT * from city_map;")
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+# # Print results.
+# for row in rows:
+#     st.write(f"{row[0]} has a :{row[1]}:")
