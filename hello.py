031d554c0a3267a317eac623443dab57e47d8f56
gong_hero <46081369+gonghaochen@users.noreply.github.com>
2022-07-29 11:03:01 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import mysql.connector
 import numpy as np
 import pandas as pd
 from bokeh.plotting import figure
@@ -20,3 +21,28 @@ df = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(df)  # Same as st.write(df)
+
+
+
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from city_map;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
